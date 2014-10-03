5d7daad718611af0105a578e0b03e98f654ec76d
aar11 <aaron.connolly08@gmail.com>
2014-10-03 15:43:42 +0100

    counting cpus

--- week3/script.sh
+++ week3/script.sh
@@ -1,3 +1,11 @@
-#this is comment
-word count is cool
-grep processor /proc/cpuinfo | wc - 1
+#!/bin/bash
+#: Title : Script
+#: Date : 3rd Oct
+#: Author : Aaron
+#: Version : 1
+#: Description : Counts number of cpu"S
+#: Options : None
+
+
+cpucount$ ( grep processor /proc/cpuinfo | wc - 1 )
+printf "my name is aaron" $cpucount
